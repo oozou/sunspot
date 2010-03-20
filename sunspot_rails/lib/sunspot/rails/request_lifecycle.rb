6587a6ad76aca4ce0a9227353ff669200f3413a9
tritonrc <brian@realitycontainment.com>
2010-03-19 18:02:38 -0600

    First pass at making sunspot_rails compatible with Rails3

--- sunspot_rails/lib/sunspot/rails/request_lifecycle.rb
+++ sunspot_rails/lib/sunspot/rails/request_lifecycle.rb
@@ -1,14 +1,14 @@
 module Sunspot #:nodoc:
   module Rails #:nodoc:
     # 
-    # This module adds an after_filter to ActionController::Base that commits
+    # This module adds an after callback to ActionDispatch that commits
     # the Sunspot session if any documents have been added, changed, or removed
     # in the course of the request.
     #
     module RequestLifecycle
       class <<self
-        def included(base) #:nodoc:
-          base.after_filter do
+        def enable! #:nodoc:
+          ActionDispatch::Callbacks.after do
             if Sunspot::Rails.configuration.auto_commit_after_request?
               Sunspot.commit_if_dirty
             elsif Sunspot::Rails.configuration.auto_commit_after_delete_request?
