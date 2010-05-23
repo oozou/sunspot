9336743a12064f179d71555b4e9cf3ac446b7735
Isaac Kearse <isaac@kearse.co.nz>
2010-05-23 17:02:21 +1200

    update generator and gemspec files for rails3

--- sunspot_rails/lib/generators/sunspot/sunspot_generator.rb
+++ sunspot_rails/lib/generators/sunspot/sunspot_generator.rb
@@ -1,9 +1,8 @@
-class SunspotGenerator < Rails::Generator::Base
-  
-  def manifest
-    record do |m|
-      m.template 'sunspot.yml', 'config/sunspot.yml'
-    end
+class SunspotGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+
+  def copy_sunspot_file
+    copy_file "sunspot.yml", "config/sunspot.yml"
   end
-  
+
 end
