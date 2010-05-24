7b127a9536c0182572a7de37a0d41079b7078d97
Isaac Kearse <isaac@kearse.co.nz>
2010-05-24 18:08:44 +1200

    Solr Cell attachment indexing patch
    http://outoftime.lighthouseapp.com/projects/20339/tickets/98/a/462987/solr_cell_attachment_indexing_v2.patch
    http://outoftime.lighthouseapp.com/projects/20339/tickets/98

--- sunspot/lib/sunspot/field_factory.rb
+++ sunspot/lib/sunspot/field_factory.rb
@@ -143,5 +143,18 @@ module Sunspot
         end
       end
     end
+
+    class Attachment
+      def initialize(name = nil, &block)
+        if block
+          @data_extractor = DataExtractor::BlockExtractor.new(&block)
+        else
+          @data_extractor = DataExtractor::AttributeExtractor.new(name)
+  end
+end
+
+      def populate_document(document, model)
+      end
+    end
   end
 end
